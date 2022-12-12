7a4008a8ca89218247e87930de64aee0c2bee096
arystan_sss <40126924+Arystan1221@users.noreply.github.com>
2022-12-12 18:22:10 +0600

    Delete confg directory

--- confg/confg.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	return $confg = array(
-"host"=>"localhost:3306",  
-"user"=>"root",  
-"pwd" =>"root",             
-"db_name" =>"db_up",            
-"tb_name" =>"tb_game",            
-);
